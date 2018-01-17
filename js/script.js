afc03bb5aa3c4973affa8ff0f9cd37525097816f
Michael Manney <michaelmanney@gmail.com>
2018-01-17 18:50:07 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	var APP = {
+		bindEvents: function() {
+			APP.tag.modalBtn.click(APP.hide)
+		},
+
+		tag: {
+			modalBtn: $('#modalBtn'),
+			hidden: $('#hidden')
+		},
+
+		nav: {
+
+		},
+
+		init: function() {
+			APP.bindEvents()
+		},
+
+		isCollapsed: false,
+
+		hide: function() {
+			/**/
+			if(APP.toggleFlag === false) {
+				APP.tag.modalBtn.html('Show Recent Build')
+				APP.toggleFlag = true
+			} else {
+				APP.tag.modalBtn.html('Hide Recent Build')
+				APP.toggleFlag = false
+			}
+			/**/
+		}
+	APP.init()
+})
