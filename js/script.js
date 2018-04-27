3220ae0f3e713a0873364438a70bfb9807b6dc2c
mjmanney <michaelmanney@gmail.com>
2018-04-26 22:51:13 -0400

    animations, smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
+
 $(document).ready(function() {
+window.scroll(0, document.documentElement.scrollHeight)
+
+	//
+	var onObserve = function (entries, observer) {
+		entries.forEach(entry => {
+			var el = $(entry.target)
+			if(entry.isIntersecting) {
+				if(!el.hasClass('animated fadeInLeft') && el.hasClass('flex-about')) el.addClass('animated fadeInLeft')
+				if(!el.hasClass('animated fadeInRight') && el.hasClass('flex-technical')) el.addClass('animated fadeInRight')
+			}
+		})
+	}
+	var options = {
+		root: null,
+		margin: '0px',
+		threshold: 0.8
+	}
+	var observer = new IntersectionObserver(onObserve, options)
+
+
 	var checkbox = $('#checkbox')
 	var label    = $('collapse-menu')
 
+	observer.observe($('.flex-technical')[0])
+	observer.observe($('.flex-about')[0])
+
+	//
+
+
+	$('a.smooth-scroll').each(function(i) {
+		let self = $(this)
+		let target = $(self.attr('href'))
+		self.on('click', function(e) {
+			e.preventDefault()
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 800, function(){
+				window.location.hash = target
+			})
+		})
+	})
 
 	checkbox.change(function() {
 		if(!checkbox.is(':checked')) {
