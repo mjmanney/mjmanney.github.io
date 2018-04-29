c5635979674da21ca4db553cccb492defdfc3471
mjmanney <michaelmanney@gmail.com>
2018-04-28 20:54:00 -0400

    responsive updates, animations

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 
 $(document).ready(function() {
-window.scroll(0, document.documentElement.scrollHeight)
+	// Scroll the bottom of the page
+	window.scroll(0, document.documentElement.scrollHeight)
 
-	//
+	// Callback for IntersectionObserver
 	var onObserve = function (entries, observer) {
 		entries.forEach(entry => {
 			var el = $(entry.target)
@@ -12,44 +13,59 @@ window.scroll(0, document.documentElement.scrollHeight)
 			}
 		})
 	}
+
+	// Options for IntersectionObserver
 	var options = {
 		root: null,
 		margin: '0px',
 		threshold: 0.8
 	}
+
+	// Instantiate observer, observe targets
 	var observer = new IntersectionObserver(onObserve, options)
-
-
-	var checkbox = $('#checkbox')
-	var label    = $('collapse-menu')
-
 	observer.observe($('.flex-technical')[0])
 	observer.observe($('.flex-about')[0])
 
-	//
-
-
-	$('a.smooth-scroll').each(function(i) {
-		let self = $(this)
-		let target = $(self.attr('href'))
-		self.on('click', function(e) {
-			e.preventDefault()
-			$('html, body').animate({
-				scrollTop: target.offset().top
-			}, 800, function(){
-				window.location.hash = target
-			})
-		})
-	})
+	// Toggle menu
+	var checkbox = $('#checkbox')
 
 	checkbox.change(function() {
 		if(!checkbox.is(':checked')) {
-			$('.nav_list_wrapper').css('display', 'block')
-		} else {
-			$('.nav_list_wrapper').css('display', 'none')
-		}
-
+			$('#modal-menu').addClass('modal-menu')
+			$('body').css('overflow', 'hidden')
+		} else {window.alert('unchecked')}
 	})
+
+	// Smooth scrolling between anchors
+	$('a.smooth-scroll').each(function(i) {
+		let self = $(this)
+		let target = $(self.attr('href'))
+
+		// TODO MAKE THIS WORK
+
+		self.on('click', function(e) {
+			if(target === "") {
+				$('#modal-menu').removeClass('modal-menu')
+				$('#checkbox').prop('checked', true)
+				$('body').css('overflow', 'scroll')
+				e.preventDefault();
+				return
+			}
+			else {
+
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 800, function(){
+					window.location.hash = target
+				})
+				$('#modal-menu').removeClass('modal-menu')
+				$('#checkbox').prop('checked', true)
+				$('body').css('overflow', 'scroll')
+								e.preventDefault()
+			}
+		})
+	})
+
 
 	var quoteList = [
 		{ person: 'Elon Musk' , quote: "Work like hell. I mean you just have to put in 80 to 100 hour weeks every week... even if you're doing the same thing [as others] you know that you will achieve in four months what it takes them a year to achieve." },
