8fdb4fa661946efc26d00a5451b3f5139bb577a2
mjmanney <michaelmanney@gmail.com>
2018-05-07 17:16:47 -0400

    google map embedded api, contact links

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var App = {
 		App.showcase()
 	},
 
-	observe: function() {
+	observe: function () {
 		var options  = { root: null, margin: '0px', threshold: 0.8 }
 		var observer = new IntersectionObserver(onObserve, options)
 		function onObserve(entires, observer) {
@@ -26,13 +26,13 @@ var App = {
 		$('#checkbox').change(function() {
 			if(!$('#checkbox').is(':checked')) {
 				$('#modal-menu').css('display', 'block')
-												.removeClass('animated slideOutUp')
-												.addClass('modal-menu animated slideInDown')
+								.removeClass('animated slideOutUp')
+								.addClass('modal-menu animated slideInDown')
 				$('body').css('overflow', 'hidden')
 			} else { window.alert('Error toggling menu.') }
 		})
 
-		$('a.smooth-scroll').each(function(i) {
+		$('a.smooth-scroll').each(function (i) {
 			var self   = $(this)
 			var target = $(self.attr('href'))
 
@@ -55,7 +55,7 @@ var App = {
 		})
 	},
 
-	showcase: function() {
+	showcase: function () {
 		var vm = new Vue({
 			el: '#projects',
 			data: {
@@ -63,12 +63,12 @@ var App = {
 					{ t: 'Weather Now', d: 'An application using Open Weather API for accurate and up-to-date weather forecasts.'                                                                   , l: '#', s:'Coming Soon', tags: ['JavaScript', 'JSON', 'API', 'AJAX']             , src:"https://images.pexels.com/photos/906023/pexels-photo-906023.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
 					{ t: 'Instascrape', d: 'A tool for developers to access public media endpoints on Instagram without using the official API or authentication.'                                  , l: '#', s:'Version 1.0', tags: ['CSS3 Flexbox', 'Promises', 'REST', 'AJAX']           , src:"https://images.pexels.com/photos/122383/pexels-photo-122383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
 					{ t: 'Arcade'     , d: 'A collection of classic arcade games built using the languages of the web - with a twist.'                                                              , l: '#', s:'Coming Soon', tags: ['Event Driven JS', 'Collision Detection', 'HTML5 Canvas', ]                       , src:"https://images.pexels.com/photos/735911/pexels-photo-735911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
-					{ t: 'Portfolio'  , d: 'The site you are on now! I am constantly updating and adding new features.  Check out the first iteration of this site see the difference!'             , l: '#', s:'Time Travel', tags: ['AWS S3', 'jQuery', 'Bootstrap', 'Vue.js' , 'Animate.css', 'Responsive Design']             , src:"https://images.pexels.com/photos/247791/pexels-photo-247791.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" }
+					{ t: 'Portfolio'  , d: 'The site you are on now! I am constantly revising and building new features.  Check out the first release to see the improvements I made.'              , l: '#', s:'Time Travel', tags: ['AWS S3', 'jQuery', 'Bootstrap', 'Vue.js' , 'Animate.css', 'Responsive Design']             , src:"https://images.pexels.com/photos/247791/pexels-photo-247791.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" }
 				],
 				c: 0
 			},
 			methods: {
-				next: function() {
+				next: function () {
 					var current = document.getElementsByClassName('i')
 					var len     = this.p.length
 					current[this.c].className = 'i'
@@ -76,7 +76,7 @@ var App = {
 					if(this.c >= len) this.c = 0
 					current[this.c].className += ' current'
 				},
-				prev: function() {
+				prev: function () {
 					var current = document.getElementsByClassName('i')
 					var len     = this.p.length
 					current[this.c].className = 'i'
@@ -86,6 +86,9 @@ var App = {
 				}
 			}
 		})
+	},
+	mapControls: function() {
+		// intercept and prevent user clicks and drags on map?
 	}
 }
 
